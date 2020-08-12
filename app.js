ba545d461b47d8af8bb37dc66fa80ba4fbed7f97
thewingngo <69607415+thewingngo@users.noreply.github.com>
2020-08-13 04:14:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAR3b5aHjEG0-FFGFPUB45Q2RTnabsLJ0c",
+  authDomain: "thewingngo-224ab.firebaseapp.com",
+  databaseURL: "https://thewingngo-224ab.firebaseio.com",
+  projectId: "thewingngo-224ab",
+  storageBucket: "thewingngo-224ab.appspot.com",
+  messagingSenderId: "418263706891",
+  appId: "1:418263706891:web:06c719627bb86583e2c60e",
+  measurementId: "G-LG9FL6793Z"
 };
 
      
